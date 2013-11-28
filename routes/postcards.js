e35234cdbea75e900d6d8de00297d39a75b3932d
Amanda Thurman <athurman215@gmail.com>
2013-11-28 06:43:10 -0600

    change over url for production in postcards.js printpdf function

--- routes/postcards.js
+++ routes/postcards.js
@@ -98,5 +98,5 @@ exports.createPDF = function(req, res){
 
 exports.printPDF = function(req, res){
   console.log(req.params.id);
-  wkhtmltopdf('http://localhost:3000/postcards/' + req.params.id + '/print').pipe(res);
+  wkhtmltopdf('http://southern-greetings.com/postcards/' + req.params.id + '/print').pipe(res);
 };
